0c5889231a9b8014a915ebcc7c3a6ea3456e85b5
Ahsan Ali <ahsanbutt515@icloud.com>
2022-09-15 15:18:37 +0500

    its in development so only my id can login

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,40 @@
-import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import store from '../config/store';
-import 'antd/dist/antd.css'
-import 'react-tippy/dist/tippy.css';
-import { createWrapper } from 'next-redux-wrapper';
-function MyApp({ Component, pageProps }) {
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import store from "../config/store";
+import "antd/dist/antd.css";
+import "react-tippy/dist/tippy.css";
+import { createWrapper } from "next-redux-wrapper";
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <ChakraProvider>
+    <SessionProvider session={session}>
+      <ChakraProvider>
         <Component {...pageProps} />
-    </ChakraProvider>
-  )
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
 // initialize store and wrapper store
-const makeStore = () => store
-const wrapper = createWrapper(makeStore)
-export default wrapper.withRedux(MyApp)
+const makeStore = () => store;
+const wrapper = createWrapper(makeStore);
+export default wrapper.withRedux(MyApp);
+
+// import '../styles/globals.css'
+// import { ChakraProvider } from '@chakra-ui/react'
+// import store from '../config/store';
+// import 'antd/dist/antd.css'
+// import 'react-tippy/dist/tippy.css';
+// import { createWrapper } from 'next-redux-wrapper';
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider>
+//         <Component {...pageProps} />
+//     </ChakraProvider>
+//   )
+// }
+
+// // initialize store and wrapper store
+// const makeStore = () => store
+// const wrapper = createWrapper(makeStore)
+// export default wrapper.withRedux(MyApp)
