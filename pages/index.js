35cce1cf2adbe742aa1a37fb88820ac41d7f8f68
Ahsan Ali <ahsanbutt515@icloud.com>
2022-09-17 14:58:49 +0500

    sample rating5

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import CallToAction from "../src/components/CallToAction";
 import FaqsComponent from "../src/components/faqsComponent/FaqsComponent";
 import RatingComponent from "../src/components/ratingComponent/RatingComponent";
 import SliderComponent from "../src/components/sliderComponent/SliderComponent";
-import styles from "../styles/Index.module.css";
+import Layout from '../src/Layout'
+
 
 export default function Home() {
   return (
@@ -14,13 +15,13 @@ export default function Home() {
         <title>Bis Resume App</title>
         <meta name="description" content="Create resume with ease!" />
       </Head>
-      <div>
+      <Layout >
         <CallToAction />
         {/* <div>Ahsan Ali</div> */}
         <FaqsComponent />
         {/* <div></div> */}
         <SliderComponent />
-      </div>
-    </div>
-  );
+      </Layout>
+    </>
+  )
 }
