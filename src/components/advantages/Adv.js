8ed2794001cf3bee8e74f13de2fa0797b1d26d46
noman-sheikh-09 <nomanshk310@gmail.com>
2022-09-19 19:01:42 +0500

    'design'

--- src/components/advantages/Adv.js
+++ src/components/advantages/Adv.js
@@ -1,15 +1,51 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
-
-export default function Adv({title,advNum}) {
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
+export default function Adv({ title, advNum, advDesc }) {
   return (
     <div>
-      <Box display="flex" flexDir="row" alignItems="center" bgColor="white" p="15px" pr="5rem" borderRadius="7px" boxShadow='md' mt="15px">
-        <Text  color="#002D6B"  bgColor="#EFC778" fontSize="1rem" fontWeight="bold" pr="8px" pl="8px" borderRadius="100%">
-       {advNum}
-        </Text>
-        <Text  color="#002D6B"  fontSize="1rem" fontWeight="bold" pl="10px">{title}</Text>
-      </Box>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Box
+              w="100%"
+              display="flex"
+              flexDir="row"
+              alignItems="center"
+              bgColor="white"
+              p="15px"
+              pr="5rem"
+              borderRadius="7px"
+              boxShadow="md"
+            >
+              <Text
+                color="#002D6B"
+                bgColor="#EFC778"
+                fontSize="1rem"
+                fontWeight="bold"
+                pr="8px"
+                pl="8px"
+                borderRadius="100%"
+              >
+                {advNum}
+              </Text>
+              <Text color="#002D6B" fontSize="1rem" fontWeight="bold" pl="10px">
+                {title}
+              </Text>
+            </Box>
+          </AccordionButton>
+
+          <AccordionPanel>
+            <Text bgColor={"white"}>{advDesc}</Text>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
--- src/components/advantages/Advantages.js
+++ src/components/advantages/Advantages.js
@@ -28,12 +28,36 @@ export default function Advantages() {
             <Image src="/adv.png" width={250} height={460} />
           </Box>
           <Box>
-            <Adv advNum="1" title="Professional, customizable templates" />
-            <Adv advNum="2" title="Expertly crafted phrases" />
-            <Adv advNum="3" title="Popular format downloads" />
-            <Adv advNum="4" title="Optimize your resume" />
-            <Adv advNum="5" title="Customize your resume to the job" />
-            <Adv advNum="6" title="Up your application game" />
+            <Adv
+              advNum="1"
+              title="Professional, customizable templates"
+              advDesc="Stand out with My Perfect Resumes eye-catching templates! Building an impressive resume has never been easier or faster."
+            />
+            <Adv
+              advNum="2"
+              title="Expertly crafted phrases"
+              advDesc="Dont get stuck writing about your work experience. Our Resume Builder has pre-written, industry-specific bullet points to create an impressive resume."
+            />
+            <Adv
+              advNum="3"
+              title="Popular format downloads"
+              advDesc="Need your resume in PDF, word, or an image file? You are in control with multiple export and download options."
+            />
+            <Adv
+              advNum="4"
+              title="Optimize your resume"
+              advDesc="With resume score and resume check, we will rate the strength of your resume and guide you through actionable steps to create a resume that showcases your best professional self!"
+            />
+            <Adv
+              advNum="5"
+              title="Customize your resume to the job"
+              advDesc="Copy and paste a job description from any listing and get a job-specific resume match score. We will guide you through tailoring your resume for the job!"
+            />
+            <Adv
+              advNum="6"
+              title="Up your application game"
+              advDesc="With Smart Apply, we will help you find an opportunity, customize your resume, create a matching cover letter, and download your documents. We will even send you to the web page where you can apply!"
+            />
           </Box>
         </Box>
       </Box>
