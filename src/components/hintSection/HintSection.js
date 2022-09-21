12774b37a2bb36d6c73a136e34307afba8ae302c
noman-sheikh-09 <nomanshk310@gmail.com>
2022-09-21 14:12:06 +0500

    'design'

--- src/components/hintSection/HintSection.js
+++ src/components/hintSection/HintSection.js
@@ -1,127 +1,82 @@
-import { Box, Image, Text } from "@chakra-ui/react";
-import React from "react";
-
-export default function HintSection({
-  message,
-  description,
-  number,
-  imgSrc,
-  isRight,
-}) {
+import { Text } from '@chakra-ui/react'
+import React from 'react'
+import { FaMinus } from "react-icons/fa";
+import Hint from './Hint';
+import styles from '../../../styles/Index.module.css'
+export default function HintSection() {
   return (
-    <Box
-      display="flex"
-      flexDir={["column", "column", isRight ? "row" : "row-reverse"]}
-      alignItems="center"
-      justifyContent={["space-between", "space-between", "space-between"]}
-      mt="2.5%"
-      mr="15%"
-      ml={[
-        isRight ? "5%" : "5%",
-        isRight ? "5%" : "5%",
-        isRight ? "5%" : "5%",
-        isRight ? "4%" : "0.3%",
-        isRight ? "0%" : "0%",
-        isRight ? "5%" : "5%",
-      ]}
-      w={["90%", "90%", "90%", "90%", "90%", "90%"]}
-      bgColor={["#F5F5F5", "#F5F5F5", "#FFFFF", "white"]}
-      borderRadius="20px"
-    >
-      <Box
-        w={["100%", "100%", isRight ? "40%" : "50%"]}
-        ml={[
-          "5",
-          "0%",
-          "0%",
-          isRight ? "10%" : "1%",
-          isRight ? "15%" : "10px",
-          isRight ? "15%" : "10px",
-        ]}
-        mr={[isRight ? "0%" : "5%", , , , isRight ? "0%" : "10%"]}
-        mb={["5%", "5%", "5%", "5%", "5%", isRight ? "3%" : "6%"]}
-      >
-        <Text
-          display={["none", "none", "none", "block"]}
-          w={isRight ? "25%" : "25%"}
-          fontWeight="bold"
-          fontSize={[
-            isRight ? "4rem" : "4rem",
-            "4rem",
-            "6rem",
-            "4rem",
-            "4rem",
-            "6rem",
-          ]}
-          color="white"
-          bgColor={isRight ? "#51E2C2" : "#4BCBED"}
-          align="center"
-          pt={["5px"]}
-          pb="5px"
-          borderRadius="20px"
-          mb={[
-            "20px",
-            "10px",
-            "10px",
-            "0px",
-            "20px",
-            isRight ? "55px" : "18px",
-          ]}
-          ml={isRight ? "73%" : "0%"}
-        >
-          {number}
-        </Text>
-        <Box
-          bgColor={["none", "none", "none", "#F5F5F5"]}
-          borderRadius="20px"
-          mr="10px"
-          p={["10px", "20px", "30px", "30px"]}
-          mt={[, , , "5%", , ,]}
-        >
+    <div style={{ marginTop: "5%", marginBottom: "5%" }}>
           <Text
-            fontSize={["1.2rem", "2rem", "", "", "", "3rem"]}
+            fontSize={["2rem", "2rem", "2.3rem"]}
+            color="#313B47"
             fontWeight="bold"
-            color=" #313B47"
-            pt="10px"
-            textAlign="left"
+            textAlign="center"
+            pl={["7%", "14%"]}
+            pr={["14%"]}
           >
-            {message}
+            Build Your Resume Fast and Easy.
           </Text>
+          <div className={styles.hrLine}>
+            <FaMinus
+              style={{
+                color: "#1dbfaf",
+                fontSize: "30px",
+                backgroundColor: "#1dbfaf",
+                paddingLeft: "2.5%",
+                paddingRight: "2.7%",
+                borderRadius: 50,
+                height: 5,
+                textAlign: "center",
+                marginLeft: "47.5%",
+                marginTop: "0%",
+              }}
+            />
+          </div>
           <Text
-            fontSize={["1rem", "1rem", "", "", "", "2rem"]}
-            color=" #313B47"
-            mt={[, , "0px", "10px"]}
-            fontWeight="500"
+            color="#313B47"
+            fontSize={["1rem", "1.5rem"]}
+            fontWeight={["400", "400"]}
+            align="center"
+            pl={["7%", "14%"]}
+            pr={["14%"]}
+            mt="3%"
           >
-            {description}
+            ` Novoresume is lightning fast. There is no software to download. No
+            multi-part sign-up form. No long-winded tutorials. Just a
+            straightforward process.`
           </Text>
-        </Box>
-      </Box>
-      <Box
-        w={["100%", "100%", "50%"]}
-        ml={[
-          "0%",
-          "15%",
-          isRight ? "0%" : "0%",
-          isRight ? "0%" : "13%",
-          isRight ? "0%" : "15%",
-          isRight ? "0%" : "17%",
-        ]}
-      >
-        <Image
-          src={imgSrc}
-          objectFit="fill"
-          w={[
-            "100%",
-            "450px",
-            "450px",
-            "450px",
-            isRight ? "500px" : "800px",
-            isRight ? "700px" : "700px",
-          ]}
-          h={["auto", "400px", "400px", "400px", "400px", "600px"]}
-        />
-      </Box>
-    </Box>
-  );
+          <Hint
+            number="1"
+            message="Pick a Template"
+            description="`Dont sabotage your job search before it has even begun. Choose from our ATS-friendly, hand-crafted resume templates`"
+            imgSrc="/one.png"
+            isRight={true}
+            paddingBottom="30px"
+          />
+          <Hint
+            number="2"
+            message="Customize Your Layout"
+            description="`Make the resume template truly your own. Customize the layout based on your experience level.`"
+            imgSrc="/two.png"
+            isRight={false}
+            paddingBottom="20px"
+          />
+          <Hint
+            number="3"
+            message="Fill in the Blanks"
+            description="`Fill in your resume information, let our AI content analyzer do its job, and see your resume changes dynamically in real time.`"
+            imgSrc="/three.png"
+            isRight={true}
+            paddingBottom="20px"
+          />
+          <Hint
+            number="4"
+            message="Hit Download!'"
+            description="`And yes, its free! We dont hit you with a paywall once you have completed your resume in the Basic Account. If you use any of our premium features, the software will let you know about it`."
+            imgSrc="/four.jpg"
+            isRight={false}
+            paddingBottom="5px"
+          />
+        </div>
+  )
 }
