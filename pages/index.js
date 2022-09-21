206aa199b87b842de5744a4af35c22c80cb66895
noman-sheikh-09 <nomanshk310@gmail.com>
2022-09-21 22:36:55 +0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import CallToAction from "../src/components/CallToAction";
 import HintSection from "../src/components/hintSection/HintSection";
 import { Text } from "@chakra-ui/react";
 import Layout from "../src/Layout";
-
-import Hint from "../src/components/hintSection/Hint";
+import { FaMinus } from "react-icons/fa";
+import Advantages from "../src/components/advantages/Advantages";
 export default function Home() {
   return (
     <>
@@ -15,6 +15,7 @@ export default function Home() {
       <Layout>
         <CallToAction />
         <HintSection />
+        <Advantages />
       </Layout>
     </>
   );
