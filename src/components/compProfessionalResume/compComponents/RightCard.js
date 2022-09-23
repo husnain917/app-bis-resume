ccff51d8f77baeea823eab30bfc16837f562f670
Muhammad Hamza <hamzashafique535@gmail.com>
2022-09-23 16:08:09 +0500

    set letter spaceing f card

--- src/components/compProfessionalResume/compComponents/RightCard.js
+++ src/components/compProfessionalResume/compComponents/RightCard.js
@@ -20,23 +20,23 @@ export default function RightCard({ cardData}) {
         </Text>
       </Box>
         {cardData.text === "1" ?
-          <Text fontSize="17px" textAlign="justify" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
+          <Text fontSize="17px" wordSpacing ="1px" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
             Keep this simple:just your name, a professional email address, and your city and state of residence. Your resume header can also include links to a portfolio website or online job networking profile, like LinkedIn.
           </Text> :
           cardData.text === "2" ?
-            <Text fontSize="17px" textAlign="justify" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
+            <Text fontSize="17px" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
               A <Link href='/test' style={{ fontWeight: "800", color: "#0058ac", fontFamily: "BlinkMacSystemFont" }}>professional summary</Link> should explain your top abilities and work experiences in two to three sentences. A good resume summary is an elevator pitch that communicates your value and strengths as an employee and compels an employer to keep reading.
             </Text> :
             cardData.text === "3" ?
-              <Text fontSize="17px" textAlign="justify" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
+              <Text fontSize="17px" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
                 In reverse-chronological order (current or most recent job first), list your <Link href='/test' style={{ fontWeight: "800", color: "#0058ac", fontFamily: "BlinkMacSystemFont" }}>work history </Link> with your job title, company and dates of employment. Using only three to five bullet points under each resume work experience entry, highlight notable work achievements. Examples of successful projects and contributions make a bigger impression than just simply listing daily tasks.
               </Text> :
               cardData.text === "4" ?
-                <Text fontSize="17px" textAlign="justify" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
+                <Text fontSize="17px" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
                   To write the best <Link href='/test' style={{ fontWeight: "800", color: "#0058ac", fontFamily: "BlinkMacSystemFont" }}>resume skills section</Link> , find the required skills from the job posting, match them to your own abilities, and list them in bullet points. Feature a mix of hard skills (e.g., specific software programs or training-based knowledge) and important soft skills, such as attention to detail or good communication.
                 </Text> :
                 cardData.text === "5" &&
-                <Text fontSize="17px" textAlign="justify" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
+                <Text fontSize="17px" fontFamily="'Nunito',Helvetica,Arial,sans-serif" color="#002d6b" mt={2}>
                   Present your most advanced education credentials (e.g., college degree or high school diploma) in reverse-chronological order, along with the name of the institution where you earned your credential. If you have advanced training in areas that are related to the job you’re applying for (e.g., a software certification), list these courses under this resume section as well.
                 </Text>
         }
