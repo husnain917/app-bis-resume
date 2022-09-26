212f1e3e5d261f6d514a865fa093ce832578b831
Ahsan Ali <ahsanbutt515@icloud.com>
2022-09-26 17:41:04 +0500

    Add finally to git

--- pages/_app.js
+++ pages/_app.js
@@ -22,22 +22,3 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 const makeStore = () => store;
 const wrapper = createWrapper(makeStore);
 export default wrapper.withRedux(MyApp);
-
-// import '../styles/globals.css'
-// import { ChakraProvider } from '@chakra-ui/react'
-// import store from '../config/store';
-// import 'antd/dist/antd.css'
-// import 'react-tippy/dist/tippy.css';
-// import { createWrapper } from 'next-redux-wrapper';
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <ChakraProvider>
-//         <Component {...pageProps} />
-//     </ChakraProvider>
-//   )
-// }
-
-// // initialize store and wrapper store
-// const makeStore = () => store
-// const wrapper = createWrapper(makeStore)
-// export default wrapper.withRedux(MyApp)
