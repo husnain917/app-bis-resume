4f24dea20d710ab57b7e7effc4aa6fc713bb9f67
Muhammad Hamza <hamzashafique535@gmail.com>
2022-09-30 00:17:07 +0500

    design ceo template

--- pages/templates/templateCEO.js
+++ pages/templates/templateCEO.js
@@ -2,23 +2,36 @@ import React from 'react'
 import { Box } from '@chakra-ui/react'
 import Profile from '../../src/components/template1/Profile'
 import { Text } from '../../src/components/template1'
+import { useSelector } from 'react-redux'
 
 export default function templateCEO() {
+    const resumeData = (store => store.editorReducer.resumeData);
 
     return (
         <Box bg='#f6f5f0' minHeight="fit-content" minW="fit-content" px={90} py={50} >
-            <Box maxW="1100px" minW="1100px" display="flex">
-                <Box bg='#1c2125' width="300px" padding="0 40px">
-                    <Box height={300} py={10}>
+            <Box maxW="1100px" minW="1100px" display="flex" style={{ height: '100%' }}>
+                <Box bg='#1c2125' width="270px" padding="0 25px" style={{ minHeight: '100%' }}>
+                    <Box height={270} py={10}>
                         <Box border="7px solid #ffffff" width={220} height={220} borderRadius="100%">
                         </Box>
-                        <Box>
-                            <p>Contact</p>
-                                
-                        </Box>
+                    </Box>
+                    <Box>
+                        <Text
+                            value={resumeData?.contact?.heading}
+                            placeholder="Contact"
+                            customclass={"contact-f-heading-title"}
+                            path={"contact.heading"}
+                            editable={false}
+                        />
+                        <Text
+                            value={resumeData?.objective?.body}
+                            placeholder="Summary Detail"
+                            customclass={`${style.managerFName} ${style.managerTemplateTextBold}`}
+                            path={"objective.body"}
+                        />
                     </Box>
                 </Box>
-                <Box bg='#ffffff' width="800px" padding='80px 30px'>
+                <Box bg='#ffffff' width="830px" padding='80px 30px' style={{ minHeight: '100%' }}>
                     <Profile cId='ceo' />
                 </Box>
             </Box>
