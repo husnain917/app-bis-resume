98d13ce0c3f80d03b6ab8ddd308631f217df14ad
Muhammad Hamza <hamzashafique535@gmail.com>
2022-10-06 22:49:48 +0500

    solved conflict

--- pages/templates/templateCEO.js
+++ pages/templates/templateCEO.js
@@ -18,7 +18,7 @@ import EducationHistory from '../../src/components/templateceo/EducationHistory'
 import Image from 'next/image'
 import useStoreData from '../../src/components/templateCEO/useStoreData'
 
-export default function templateceo() {
+export default function Templateceo() {
    const {
         resumeData,
             theme,
