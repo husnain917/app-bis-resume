f39c6fada7c08dca5fac77c79cf37f21fe805279
M Abubakar Raza <abubakarraza64@gmail.com>
2022-11-01 13:12:09 +0500

    update Templates

--- pages/templates/template5.js
+++ pages/templates/template5.js
@@ -19,99 +19,114 @@ import ExperienceTemplate5 from '../../src/components/template5/ExperienceTempla
 import EducationTemplate5 from '../../src/components/template5/EducationTemplate5';
 import AboutTemplate5 from '../../src/components/template5/AboutTemplate5';
 import CertificationTemplate5 from '../../src/components/template5/CertificationTemplate5';
+import Link from 'next/link';
+import style from '../../styles/templates/template1.module.scss';
+import { Tooltip } from 'react-tippy';
 const template5 = (props) => {
   const { resumeData } = props;
   console.log('resumeData:', resumeData);
   return (
-    <Flex justifyContent={{ base: 'none', lg: 'center' }}>
-      <SimpleGrid minW={850} bg={'blackAlpha.300'} py={12}>
-        <HStack>
-          <VStack
-            align={'flex-end'}
-            justify="start"
-            lineHeight="34px"
-            pr={6}
-            w="399px"
-          >
-            <ProfileTemplate5 />
-          </VStack>
+    <div className={style.main}>
+      <Link href={'/templates'}>
+        <div style={{ position: 'fixed', top: '30px', right: '30px' }}>
+          <Tooltip title="Change Template" arrow distance={20}>
+            <div className={style.swap}>
+              <img src="/icons/swap.png" />
+            </div>
+          </Tooltip>
+        </div>
+      </Link>
 
-          <VStack h={'full'} width="2px" bg="black"></VStack>
-
-          <VStack align={'right'} pl={6} w={370} pr={6}>
-            <Heading size={'lg'}>About Me</Heading>
-            <AboutTemplate5 />
-          </VStack>
-        </HStack>
-        <HStack>
-          <Heading size={'lg'} ml="45px" mb={10}>
-            Experience
-          </Heading>
-          <Stack width={565}>
-            <Divider borderColor={'black'} />
-          </Stack>
-        </HStack>
-        <VStack mt={-5}>
-          <ExperienceTemplate5
-            data={
-              resumeData?.work?.items.length
-                ? [...resumeData?.work?.items]
-                : [...sampleData?.data?.work?.items]
-            }
-          />
+      <Flex justifyContent={{ base: 'none', lg: 'center' }}>
+        <SimpleGrid minW={850} bg={'blackAlpha.300'} py={12}>
           <HStack>
-            <Heading size={'lg'} ml="-54px" w={150}>
-              Education
+            <VStack
+              align={'flex-end'}
+              justify="start"
+              lineHeight="34px"
+              pr={6}
+              w="399px"
+            >
+              <ProfileTemplate5 />
+            </VStack>
+
+            <VStack h={'full'} width="2px" bg="black"></VStack>
+
+            <VStack align={'right'} pl={6} w={370} pr={6}>
+              <Heading size={'lg'}>About Me</Heading>
+              <AboutTemplate5 />
+            </VStack>
+          </HStack>
+          <HStack>
+            <Heading size={'lg'} ml="45px" mb={10}>
+              Experience
             </Heading>
-            <Stack width={545}>
-              <Divider p={3} borderColor={'black'} />
+            <Stack width={565}>
+              <Divider borderColor={'black'} />
             </Stack>
           </HStack>
-
-          <VStack>
-            <EducationTemplate5
+          <VStack mt={-5}>
+            <ExperienceTemplate5
               data={
-                resumeData?.education?.items.length
-                  ? [...resumeData?.education?.items]
-                  : [...sampleData?.data?.education?.items]
+                resumeData?.work?.items.length
+                  ? [...resumeData?.work?.items]
+                  : [...sampleData?.data?.work?.items]
               }
             />
-          </VStack>
-          <HStack>
-            <Heading size={'lg'} ml="-54px" w={150}>
-              Certificate
-            </Heading>
-            <Stack width={545}>
-              <Divider p={3} borderColor={'black'} />
-            </Stack>
-          </HStack>
-          <CertificationTemplate5
-            data={
-              resumeData?.certifications?.items.length
-                ? [...resumeData?.certifications?.items]
-                : [...sampleData?.data?.certifications?.items]
-            }
-          />
-          <HStack>
-            <Heading w={220} size={'lg'} ml="-45px">
-              Skill & Hobbies
-            </Heading>
-            <Stack width={480}>
-              <Divider p={3} borderColor={'black'} />
-            </Stack>
-          </HStack>
-          <Box>
-            <SkillBase
+            <HStack>
+              <Heading size={'lg'} ml="-54px" w={150}>
+                Education
+              </Heading>
+              <Stack width={545}>
+                <Divider p={3} borderColor={'black'} />
+              </Stack>
+            </HStack>
+
+            <VStack>
+              <EducationTemplate5
+                data={
+                  resumeData?.education?.items.length
+                    ? [...resumeData?.education?.items]
+                    : [...sampleData?.data?.education?.items]
+                }
+              />
+            </VStack>
+            <HStack>
+              <Heading size={'lg'} ml="-54px" w={150}>
+                Certificate
+              </Heading>
+              <Stack width={545}>
+                <Divider p={3} borderColor={'black'} />
+              </Stack>
+            </HStack>
+            <CertificationTemplate5
               data={
-                resumeData?.skills?.items?.length
-                  ? [...resumeData?.skills?.items]
-                  : [...sampleData?.data?.skills?.items]
+                resumeData?.certifications?.items.length
+                  ? [...resumeData?.certifications?.items]
+                  : [...sampleData?.data?.certifications?.items]
               }
             />
-          </Box>
-        </VStack>
-      </SimpleGrid>
-    </Flex>
+            <HStack>
+              <Heading w={220} size={'lg'} ml="-45px">
+                Skill & Hobbies
+              </Heading>
+              <Stack width={480}>
+                <Divider p={3} borderColor={'black'} />
+              </Stack>
+            </HStack>
+            <Box>
+              <SkillBase
+                data={
+                  resumeData?.skills?.items?.length
+                    ? [...resumeData?.skills?.items]
+                    : [...sampleData?.data?.skills?.items]
+                }
+              />
+            </Box>
+          </VStack>
+        </SimpleGrid>
+      </Flex>
+    </div>
   );
 };
 const mapStateToProps = (store) => ({
