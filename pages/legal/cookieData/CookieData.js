45d9a7657d73b481594749e0e5fc8341496448ba
muhammadawais112 <simpleboy1917@gmail.com>
2022-11-02 04:22:09 -0700

    cookie page design

---
+++ pages/legal/cookieData/CookieData.js
@@ -0,0 +1,205 @@
+
+export const TABLE_CONTENT_DATA = [
+    {
+        label: 'What is a cookie?',
+        href: '#',
+
+    },
+    {
+        label: 'Use of cookies and web storage(browsing data) by Novorésumé',
+        href: '#',
+    },
+    {
+        label: 'Disabling/Enabling Cookies',
+        href: '#',
+    },
+]
+
+
+export const PREMIUM_ACCOUNT_DATA = [
+    {
+        label: 'Create better content that you will enjoy and will improve your job searching process & career. ',
+    },
+    {
+        label: 'Deliver you exclusive discounts, promotions and seasonal offers that will save you time and money.        ',
+    },
+    {
+        label: 'Create user personas and discover the interests of the Novorésumé users and insights to improve the content, advertisements, promotions and discounts shown to you.'
+    },
+    {
+        label: 'Show you tailored content for your region and unique examples, tips, and tutorials following the local recruiting trends.'
+    },
+    {
+        label: 'Analyze the user journey to identify key issues and pain points where we can make improvements.',
+    },
+    {
+        label: 'Target users with informative marketing campaigns related to our services.'
+    },
+]
+
+
+
+export const Marketing = [
+    {
+        label: 'conduct marketing campaigns  ',
+    },
+    {
+        label: 'limit the number of times you see an advertisement ',
+    },
+
+
+    {
+        label: 'help measure the effectiveness of the advertising campaign ',
+    },
+    {
+        label: 'create user personas '
+    },
+    {
+        label: 'discover user interests '
+    },
+]
+
+
+
+
+
+export const CONTACT_US_DATA = [
+
+
+    {
+        label: 'one',
+    },
+    {
+        label:'second',
+        description: `If you feel like something is missing or misleading in our Cookie Policy, please feel free to notify us.        `,
+    },
+
+
+    {
+        label: 'third',
+        description: 'The user should always have the freedom of choice, so if you disagree with the Cookies Policy, please do not use Novorésumé.        ',
+    },
+    {
+        label: 'thirdLink',
+        description: 'The Cookie Policy was last modified on 12/April/2021.        ',
+        mail: ''
+    },
+]
+
+
+export const TERMS_DATA = {
+    TermsOfUse: {
+        heading: [
+            {
+                title: 'Bisrésumé ApS’s'
+            },
+            {
+                title: 'Cookie Policy'
+            },
+        ],
+        data: [
+            {
+                text: 'Novorésumé welcomes you to this website. One of our goals is to provide you with the best user experience, a reason for which Novorésumé uses cookies.                ',
+            },
+            {
+                text: 'If you disagree with any part of the Cookie Policy, please note that parts of novoresume.com may be disabled. We might also use third-party cookies, which could be processed, transferred outside of EU in accordance with the European Commission’s standard contractual clauses.                ',
+            },
+
+        ],
+        description: '(Disclaimer: Novorésumé is a product of Novorésumé ApS, meaning that the Terms of Use found below applies to Novorésumé. When there is a reference to „Novorésumé”, „us” or „we” this takes into account the following: novoresume.com, Novorésumé ApS, website, site or any other application/app/service/software directly related to Novorésumé and owned & developed by Novorésumé.)'
+    },
+    TableOfContents: {
+        heading: 'Table of Contents:',
+    },
+    TheNovoresumeContent: {
+        heading: '1.What is a cookie?',
+        data: [
+            {
+                text: 'Cookies are text files containing small amounts of information which are downloaded to your device when you visit a website. Cookies are then sent back to the originating website on each subsequent visit, or to another website that recognizes that cookie.',
+            },
+            {
+                text: 'These cookies contain no personally identifiable information but they will use your computer’s IP address to know from where in the world you are accessing the Internet.'
+            },
+            {
+                text: 'Cookies do lots of different jobs, like letting you navigate between pages efficiently, remembering your preferences, and improve the user experience. They can also help to ensure that adverts you see online are more relevant to you and your interests.'
+            },
+
+        ]
+    },
+    AcceptableUse: {
+        heading: '2. Use of cookies and web storage(browsing data) by Novorésumé',
+        data: [
+            {
+                text: 'Within Novorésumé, the browsing data is used to facilitate features and functions which improve your browsing experience. They are also used to measure the performance of Novorésumé and how visitors navigate their way around. Personal, identifiable information is collected by the cookies on Novorésumé, such as:',
+            },
+            {
+                head: 'Authentication',
+                text: 'To use Novorésumé to its full potential, users need to be uniquely identified.',
+            },
+            {
+                head: 'Personal Preferences & Information',
+                text: 'To give a better user experience, Novorésumé uses the browsing data to remember the users personal preferences and information regarding Novorésumé usage.                '
+            },
+            {
+                head: 'Third-Party Cookies',
+                text: '(Novorésumé is not responsible for third-party cookies, for more information, please read the respective Cookie Policy. You can find a list of our third parties here.)'
+            },
+
+
+            {
+                text: 'To improve Novorésumés services, for a better understanding of your needs, preferences and provide you the best experience using novoresume.com,etc. Novorésumé uses third-party cookies which allows to:   '
+            },
+
+
+        ]
+    },
+
+
+    WarrantyAndPremiumAccount: {
+        heading: '3. Disabling/Enabling Cookies',
+        description: 'You can accept or decline cookies by modifying the settings in your browser. However, you will not be able to use all the interactive and helpful features of Novorésumé.        ',
+        paragraphs: [
+            {
+                text: 'Most recent web browsers allow control over the cookies saved through the browser settings.'
+            },
+            {
+                head: 'A) Necessary Cookies',
+                text: 'One of our goals is to provide you with the best user experience, a reason for which Novorésumé uses cookies. These cookies are necessary for the website to function; therefore, we cannot switch them off in our systems. They are usually only set in response to actions made by you, which amount to a request for services, such as setting your privacy preferences, logging in, or filling in forms.                '
+            },
+            {
+                head: 'B) Functional Cookies ',
+                text: 'These cookies enhance your experience on our website by allowing us to remember some of the choices you make (such as username, language, or region), thus enabling us to provide better functionality, personalization, security, and improved user experience.                '
+            },
+            {
+                text: 'If you do not allow these cookies, then some or all of these services may not function properly. '
+            },
+
+            {
+                text: 'They may be set by us or by third-party services we have added to our pages. Novorésumé is not responsible for third-party cookies. Third-party tools might also process the IP address. For more information, please read the respective Cookie Policy. You can find a list of our third parties here.                    '
+            },
+            {
+                head: 'C) Analytical Cookies ',
+                text: 'These cookies allow us to generate a better user journey and identify issues by collecting information about how visitors use our website while also helping us improve its overall performance. For instance, they could track the traffic source (e.g., whether a visitor came through an affiliate), which pages visitors go to most often, how they move around and interact with the website, if they get error messages, etc.                '
+            },
+            {
+                text: 'They may be set by us or by third-party services we have added to our pages. Novorésumé is not responsible for third-party cookies. Third-party tools might also process the IP address. For more information, please read the respective Cookie Policy. You can find a list of our third parties here.                '
+            },
+            {
+                head: 'D) Marketing Cookies',
+                text: 'These cookies help us deliver you exclusive discounts or offers tailored to your needs, to save you money and skyrocket your career. They remember that you have visited a website or performed a specific action, etc. and we might share this information with other third-party services we use. These cookies may be linked to site functionality provided by the other organization.                '
+            },
+            {
+                text: 'They are also used to:'
+            },
+
+
+
+        ],
+
+    },
+
+    ContactUs: {
+        heading: 'Contact Us',
+        subHeading: 'Novorésumé details:',
+    }
+}
