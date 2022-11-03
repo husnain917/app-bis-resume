b2ce7202c3d319badff314b5385db4194ccf7d67
Ahsan Ali <ahsanbutt515@icloud.com>
2022-11-03 23:04:39 +0500

    code Added

---
+++ pages/useImage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ImageSelector from "../src/components/imageSelector";
+
+const UseImage = () => {
+  return <ImageSelector />;
+};
+
+export default UseImage;
---
+++ src/components/imageSelector.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageSelector = () => {
+  return <div>imageSelector</div>;
+};
+
+export default ImageSelector;
