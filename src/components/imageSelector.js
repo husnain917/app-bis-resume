6859e506da8a9acc05665e32bc765842a2fa8177
Ahsan Ali <ahsanbutt515@icloud.com>
2022-11-05 13:24:55 +0500

    code Added

--- src/components/imageSelector.js
+++ src/components/imageSelector.js
@@ -45,14 +45,16 @@ const ImageSelector = () => {
               &nbsp;
               {imageList.map((image, index) => (
                 <Box key={index} className="image-item">
-                  <Image
-                    src={image.data_url}
-                    alt=""
-                    width="100"
-                    borderRadius="50%"
-                    h={"12em"}
-                    w={"12em"}
-                  />
+                  <Box className="pic_container">
+                    <Image
+                      src={image.data_url}
+                      alt="/imageProfile.jpg"
+                      width="100"
+                      borderRadius="50%"
+                      h={"16em"}
+                      w={"16em"}
+                    />
+                  </Box>
                   <Box className="image-item__btn-wrapper">
                     <button onClick={() => onImageUpdate(index)}>Update</button>
                     <button onClick={() => onImageRemove(index)}>Remove</button>
@@ -68,3 +70,70 @@ const ImageSelector = () => {
 };
 
 export default ImageSelector;
+
+// import React, { useState } from "react";
+// import { Box, Image } from "@chakra-ui/react";
+// import { EditIcon } from "@chakra-ui/icons";
+
+// const ImageSelector = () => {
+//   const [file, setFile] = useState();
+//   function handleChange(e) {
+//     console.log(e.target.files);
+//     setFile(URL.createObjectURL(e.target.files[0]));
+//   }
+
+//   return (
+//     <Box>
+//       <input type="file" onChange={handleChange} />
+//       <input type="file" id="imageUpload" accept=".png, .jpg, .jpeg" />
+//       <Box position={"absolute"}>
+//         <Box
+//           borderWidth={"1"}
+//           borderColor="black"
+//           className="pic_container"
+//           zIndex="-1"
+//         >
+//           <Box
+//             position={"absolute"}
+//             zIndex={"1"}
+//             top={"10"}
+//             right={"6"}
+//             bg="whitesmoke"
+//             h={6}
+//             w={6}
+//             display="flex"
+//             alignItems={"center"}
+//             justifyContent={"center"}
+//             borderRadius={"50%"}
+//           >
+//             <EditIcon w={4} h={4} zIndex={"0.5"} />
+//           </Box>
+
+//           {file ? (
+//             <Image
+//               src={file}
+//               alt=""
+//               width="100"
+//               borderRadius="50%"
+//               h={"16em"}
+//               w={"16em"}
+//               borderWidth={4}
+//               borderColor="black"
+//             />
+//           ) : (
+//             <Image
+//               src="./UploadImage.jpg"
+//               alt=""
+//               width="100"
+//               borderRadius="50%"
+//               h={"16em"}
+//               w={"16em"}
+//             />
+//           )}
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default ImageSelector;
