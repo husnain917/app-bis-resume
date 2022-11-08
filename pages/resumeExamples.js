baeb6a557c6ba36e44d1f99f820c9658cdb3e149
Ahsan Ali <ahsanbutt515@icloud.com>
2022-11-08 11:44:33 +0500

    resumeExamplesData

---
+++ pages/resumeExamples.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
+import ResumeExampleData from "../src/components/resumeExampleData/resumeExampleData";
+
+const ResumeExamples = () => {
+  return (
+    <Box>
+      <ResumeExampleData />
+    </Box>
+  );
+};
+
+export default ResumeExamples;
---
+++ src/components/resumeExampleData/resumeExampleData.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
+
+const ResumeExampleData = () => {
+  return <Box>resumeExampleData</Box>;
+};
+
+export default ResumeExampleData;
