7b0d59ff58f06f3e9b96d32218e0e82ccc7b0e17
Ahsan Ali <ahsanbutt515@icloud.com>
2022-11-15 16:42:12 +0500

    codeChanged

--- src/components/resumeExampleData/tryResume/tryResume.js
+++ src/components/resumeExampleData/tryResume/tryResume.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, Image } from "@chakra-ui/react";
 import { Grid } from "@chakra-ui/react";
 
 const TryResume = () => {
   return (
     <Box py={["48px", "48px", "64px", "80px", "80px"]}>
-      <Grid
-        columns={2}
-        spacing={10}
-        px={["22px", "22px", "32px", "44px", "44px"]}
-      >
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-      </Grid>
+      <Box px={["22px", "22px", "32px", "44px", "44px"]}>
+        <Box display={"flex"}>
+          <Image src="/graphicMan.jpg" className="image1" />
+          <Image src="/graphicMan.jpg" className="image2" />
+          <Image src="/graphicMan.jpg" className="image3" />
+        </Box>
+      </Box>
     </Box>
   );
 };
