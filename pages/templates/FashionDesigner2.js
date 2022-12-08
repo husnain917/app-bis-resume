d38b0d1eb357c7e801c36853beb45d7ec0b2855f
Ahsan Ali <ahsanbutt515@icloud.com>
2022-12-08 21:57:41 +0500

    FixCodeOfFinancialExpert

--- pages/templates/FashionDesigner2.js
+++ pages/templates/FashionDesigner2.js
@@ -4,7 +4,6 @@ import { BsFillLayersFill } from "react-icons/bs";
 import style from "../../styles/templates/template1.module.scss";
 import Classes from "../../styles/templates/hybridTemplate.module.css";
 import Class from "../../styles/templates/fashion2.module.scss";
-import { Tooltip } from "react-tippy";
 import Name from "../../src/components/commonSection/Name";
 import About from "../../src/components/commonSection/About";
 import Contact from "../../src/components/commonSection/Contact";
