a5939fc7e49e278bf63d40d1cc48b994ebb7d6f1
M Abubakar Raza <abubakarraza64@gmail.com>
2022-12-13 15:41:04 +0500

    solve nav issue

--- src/Layout.js
+++ src/Layout.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BuildResume from './components/BuildResume';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
-
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 export default function Layout({ children }) {
+  const router = useRouter();
+  const [show, setShowNav] = useState(true);
+  const path = router.pathname;
+  useEffect(() => {
+    if (path.includes('templates/')) {
+      setShowNav(false);
+    } else {
+      setShowNav(true);
+    }
+  }, []);
+
   return (
     <>
-      <Navbar />
+      {show && <Navbar />}
       <main>{children}</main>
       <Footer />
     </>
