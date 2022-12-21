a66f44b941d9d954386d5eb851e28f386397689e
husnain917 <honeyansari917@gmail.com>
2022-12-21 23:10:58 +0500

    change pdf library

--- src/components/tempNav/PDFGenerater.js
+++ src/components/tempNav/PDFGenerater.js
@@ -1,22 +1,14 @@
 import { useRef } from "react";
-import { jsPDF } from "jspdf";
-import html2canvas from "html2canvas";
-import { useSelector } from 'react-redux';
+import { useReactToPrint } from "react-to-print";
 
 export default function PDFGenerater() {
-    // redux data
-    let tempSize = useSelector((state) => state.editorReducer.templateSize);
-
     // pdf generater
     const pdfRef = useRef(null);
-    const downloadPDFHandler = () => {
-        html2canvas(pdfRef.current).then((canvas) => {
-            const imgData = canvas.toDataURL("image/png");
-            const pdf = new jsPDF('p', 'mm', [tempSize.tempWidth, tempSize.tempHeight]);
-            pdf.addImage(imgData, "JPEG", 0, 0);
-            pdf.save("download.pdf");
-        });
-    };
+    const downloadPDFHandler = useReactToPrint({
+        content: () => pdfRef.current,
+        documentTitle: 'download.pdf',
+        // onAfterPrint: () => alert('download success')
+    })
     return {
         pdfRef,
         downloadPDFHandler,
