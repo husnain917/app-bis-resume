e2549726ac3f3d2f58b0a6306d63a52da45ad7a4
Ahsan Ali <ahsanbutt515@icloud.com>
2022-12-24 09:35:44 +0500

    Adjusted

--- src/components/resumeExampleData/commonComponent/commonComponent.js
+++ src/components/resumeExampleData/commonComponent/commonComponent.js
@@ -70,7 +70,7 @@ const CommonComponent = ({ data }) => {
         <Box display={["none", "none", "flex", "flex", "flex"]}>
           {" "}
           <Box h={12} w={"40em"}>
-            <Text>
+            <Text fontSize={["0.6rem", "0.8rem"]}>
               {data.description ||
                 "Our tailor-made samples and guides has all of the tools you needto easily craft a government CV in just minutes. Our samples have been field-tested and are expertly designed to set you up for success."}
             </Text>
