9038e2fa5ee1cb8338d61e6134e33364f4ac4e05
Ahsan Ali <ahsanbutt515@icloud.com>
2022-12-27 18:00:50 +0500

    PageFixings

--- src/components/BuildResume.js
+++ src/components/BuildResume.js
@@ -9,14 +9,14 @@ import Link from "next/link";
 export default function BuildResume() {
   return (
     <>
-      <div className={styles.ctsMain}>
+      <Box className={styles.ctsMain} mt={5}>
         <Box id="svg-section"></Box>
-        <h1 className={styles.ctaHeading}>Ready to Jump-Start Your Career</h1>
+        <h1 className={"sub-heading"}>Ready to Jump-Start Your Career</h1>
 
-        <Box className={styles.ctaHr} mt={2} mb={3}></Box>
+        <Box className={styles.ctaHr} mt={3} mb={4}></Box>
 
-        <div>
-          <Box>
+        <Box>
+          <Box mb={2}>
             <Link href="/templates">
               {/* <Button
                 className={styles.ctaBtn}
@@ -42,14 +42,14 @@ export default function BuildResume() {
               />
             </Link>
           </Box>
-          <p>No credit card required</p>
-        </div>
-        <div></div>
-      </div>
+          <p className={"small-text"}>No credit card required</p>
+        </Box>
+        <Box></Box>
+      </Box>
 
-      <div className={styles.ctaImageDiv}>
+      <Box className={styles.ctaImageBox}>
         <Image src={ImageBuild} />
-        <div className={styles.ctaWaveBlind}>
+        <Box className={styles.ctaWaveBlind}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
               fill="	#e5e5e5"
@@ -57,8 +57,8 @@ export default function BuildResume() {
               d="M0,224L80,240C160,256,320,288,480,261.3C640,235,800,149,960,149.3C1120,149,1280,235,1360,277.3L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
             ></path>
           </svg>
-        </div>
-      </div>
+        </Box>
+      </Box>
     </>
   );
 }
