b93075b9358ded848aed18132da2de7de9d2bcca
Ahsan Ali <ahsanbutt515@icloud.com>
2023-01-05 02:23:26 +0500

    ContactUsPageFixings

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -31,26 +31,27 @@ export default function ContactUs() {
       >
         {/* inner main div  */}
         <Box w={["100%", "100%", "98%", "70%"]} h="auto" mt="5%">
-          <Text
-            fontSize={["2.9em"]}
-            fontWeight="600"
-            color="#000000"
-            pl="9%"
-            fontFamily={`EuclidCircularB-Medium', sans-serif`}
-          >
-            Contact Us
-          </Text>
-          <Text
-            color="black"
-            fontWeight="400"
-            fontSize={["1rem", "1rem", "1rem", "1rem", "1.1rem", "1.1rem"]}
-            pl="9%"
-            pr="10%"
-            fontFamily={`EuclidCircularB-Medium', sans-serif`}
-          >
-            Have comments, questions, or feedback to share? Our team would love
-            to hear from you. Give us a call or submit a message below.
-          </Text>
+          <Box paddingX={"10%"}>
+            <Text
+              fontSize={["2.6em"]}
+              fontWeight="600"
+              color="#000000"
+              className="medium-heading"
+            >
+              Contact Us
+            </Text>
+            <Text
+              color="black"
+              fontWeight="400"
+              fontSize={["1rem", "1rem", "1rem", "1rem", "1.1rem", "1.1rem"]}
+              className="medium-text"
+              lineHeight={"26px"}
+              mt={3}
+            >
+              Have comments, questions, or feedback to share? Our team would
+              love to hear from you. Give us a call or submit a message below.
+            </Text>
+          </Box>
           {/* white container  */}
 
           <Box
@@ -62,29 +63,22 @@ export default function ContactUs() {
             pl={["5%", , , "9%"]}
             pr={["5% ", , "5%"]}
           >
-            <Text
-              pt="5%"
-              fontFamily={`EuclidCircularB-Medium', sans-serif`}
-              className='small-heading'
-            >
+            <Text pt="5%" className="small-heading">
               Select a topic
             </Text>
-            <Box
-              display="flex"
-              flexWrap={'wrap'}
-              mt="2%"
-            >
+            <Box display="flex" flexWrap={"wrap"} mt="2%">
               {buttonData.map((button, index) => {
                 return (
                   <Box key={index}>
-                  <ContactButton
-                    button={button}
-                    index={index}
-                    bgColor={active === index ? "#2CACD5" : "none"}
-                    color={active === index ? "white" : "grey"}
-                    onFocunHandler={onFocunHandler}
-                  // onClick={button.link}
-                  />
+                    <ContactButton
+                      button={button}
+                      index={index}
+                      bgColor={active === index ? "#2CACD5" : "none"}
+                      color={active === index ? "white" : "grey"}
+                      onFocunHandler={onFocunHandler}
+                      className={"small-text"}
+                      // onClick={button.link}
+                    />
                   </Box>
                 );
               })}
@@ -103,11 +97,21 @@ export default function ContactUs() {
               ) : (
                 <>
                   <Box w={["100%", "100%", "50%", "50%"]} mt="3%">
-                    <CustomInput title="Email" height="50px" />
-                    <CustomInput title="Name" marginTop="5%" height="50px" />
+                    <CustomInput
+                      title="Email"
+                      height="50px"
+                      className={"small-text"}
+                    />
+                    <CustomInput
+                      title="Name"
+                      marginTop="5%"
+                      height="50px"
+                      className={"small-text"}
+                    />
                   </Box>
                   <Box w={["100%", "100%", "50%", "50%"]} mt="3%">
                     <CustomInput
+                      className={"small-text"}
                       title="Message"
                       height="150px"
                       paddingBottom="100px"
@@ -121,16 +125,14 @@ export default function ContactUs() {
               mt="5%"
               display="flex"
               flexDir={["column", "column", "row", "row"]}
-              alignItems={'center'}
+              alignItems={"center"}
               pr="5%"
             >
-              <Box
-                padding={'0px 10px 0px 0px'}
-              >
+              <Box padding={"0px 10px 0px 0px"}>
                 <Text
-                  fontSize={["1rem", "1rem", "1rem", "1rem",]}
+                  fontSize={["1rem", "1rem", "1rem", "1rem"]}
                   textAlign={["center", "center", "left", "left"]}
-                  fontFamily={`EuclidCircularB-Medium', sans-serif`}
+                  className={"medium-text"}
                 >
                   This site is protected by reCAPTCHA and the Google Privacy
                   Policy and Terms of Service apply.
@@ -140,10 +142,10 @@ export default function ContactUs() {
               <Box>
                 <CommonButton
                   height={"40px"}
-                  title={'Send Message'}
+                  title={"Send Message"}
                   hoverCursor={"pointer"}
-                  backgroundColor={'#2CACD5'}
-                  color={'white'}
+                  backgroundColor={"#2CACD5"}
+                  color={"white"}
                   hoverColor={"white"}
                   hoverBackgroundColor={"#2CACD5"}
                   margin={["5%", "5%", "5%", "0%"]}
@@ -154,8 +156,6 @@ export default function ContactUs() {
           </Box>
         </Box>
       </Box>
-
-
     </>
   );
 }
