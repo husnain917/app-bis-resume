695c77ba4df6642e23fbc8c09eae2f635cb306b8
Muhammad Sami Shoukat <samishoukat12@gmail.com>
2023-01-07 19:14:19 +0500

    style added

--- pages/ResumeHelp.js
+++ pages/ResumeHelp.js
@@ -13,7 +13,7 @@ import ExpImage from "../public/ExpImage.png"
 import jobAd from "../public/jobAd.png"
 import EducationHelpImage from "../public/EducationHelpImage.png";
 import SkillImage from "../public/SkillImage.png"
-import ResumeHel from "../public/ResumeHel.png"
+import ResumeHel from "../public/ResumeHelp.png"
 import ProfileDescrip from "../public/ProfileDescrip.png"
 import SideBar from "../src/components/sideBar/SideBar";
 import Layout from "../src/Layout";
