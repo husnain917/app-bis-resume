4e3d948cc467b5fa95e3fa287f1cdb456a05e9db
Ahsan Ali <ahsanbutt515@icloud.com>
2023-01-18 18:01:39 +0500

    resolveDoubleNavbarIssue

--- src/Layout.js
+++ src/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
 
   return (
     <>
-      {path.includes("templates/") || path.includes("blog/") ? (
+      {path.includes("templates/") || path.includes("/blog") ? (
         <>
           <main>{children}</main>
         </>
