0226f206a4a710a6b704d117724f90c6945ed219
husnain917 <honeyansari917@gmail.com>
2023-01-27 20:40:30 +0500

    fix pages UI

--- src/components/aboutUs/secondSection/data.js
+++ src/components/aboutUs/secondSection/data.js
@@ -6,12 +6,12 @@ export const data = [
       <>
         `
         <p>
-        Looking for a way to stand out in the job market? Our {" "}
+          {`Looking for a way to stand out in the job market? Our`} {" "}
           <a href="https://bisresume.com/">
             Bisresume Builder
           </a>{" "}
-          offers a cutting-edge solution for job seekers looking to create a professional and polished resume in minutes. With a wide variety of customizable templates and examples tailored to specific industries, our platform makes it easy to create a resume that showcases your skills and experience in the best possible light. Whether you're just starting out in your career or looking to make a change, our platform has everything you need to take the next step and land your dream job."{" "}
-          
+          {`offers a cutting-edge solution for job seekers looking to create a professional and polished resume in minutes. With a wide variety of customizable templates and examples tailored to specific industries, our platform makes it easy to create a resume that showcases your skills and experience in the best possible light. Whether you're just starting out in your career or looking to make a change, our platform has everything you need to take the next step and land your dream job."`} {" "}
+
         </p>
         `
       </>
@@ -28,8 +28,8 @@ export const data = [
         {" "}
         `
         <p>
-         
-        Our platform goes beyond just creating a professional resume, we're dedicated to helping individuals succeed in their career journey. Our Career Center is a one-stop shop for all your career needs, offering a wealth of resources and advice on job searching, networking, and professional development. Whether you're just starting out, looking for a new opportunity, or looking to advance in your current role, we have the tools and expertise to guide you every step of the way.
+
+          {` Our platform goes beyond just creating a professional resume, we're dedicated to helping individuals succeed in their career journey. Our Career Center is a one-stop shop for all your career needs, offering a wealth of resources and advice on job searching, networking, and professional development. Whether you're just starting out, looking for a new opportunity, or looking to advance in your current role, we have the tools and expertise to guide you every step of the way.`}
         </p>
         `
       </>
@@ -45,7 +45,7 @@ export const data = [
       <>
         `
         <p>
-        A powerful resume alone may not be enough to land your dream job. That's why we offer an easy-to-use cover letter builder to complement your resume. Our platform provides all the necessary tools and guidance to craft a compelling cover letter that highlights your strengths and qualifications. With our pre-written examples and customizable templates, you can create a professional and personalized cover letter that will capture the attention of recruiters and increase your chances of getting hired.
+          {`A powerful resume alone may not be enough to land your dream job. That's why we offer an easy-to-use cover letter builder to complement your resume. Our platform provides all the necessary tools and guidance to craft a compelling cover letter that highlights your strengths and qualifications. With our pre-written examples and customizable templates, you can create a professional and personalized cover letter that will capture the attention of recruiters and increase your chances of getting hired.`}
         </p>
         `
       </>
