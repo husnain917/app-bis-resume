6774e7f9eb403adf50a10b3d3a8a319d284065e9
husnain917 <honeyansari917@gmail.com>
2023-02-04 19:46:45 +0500

    change Ui templates pages

--- pages/templates/index.js
+++ pages/templates/index.js
@@ -1,4 +1,4 @@
-import { SimpleGrid, Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, Container } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "@chakra-ui/react";
 import { CUSTOM_TEMP_DATA } from "../../src/components/customTempData/CustomTempData";
@@ -37,15 +37,16 @@ function Templates() {
           Professional
         </Text>
       </Box>
-      <SimpleGrid
-        columns={{ base: 2, sm: 3, md: 4 }}
-        spacing={10}
+      <Container
+        maxWidth={'100%'}
+        display={'flex'}
+        flexWrap={'wrap'}
+        justifyContent={['center', 'center', 'space-evenly', 'space-evenly']}
         py={4}
-        px={10}
       >
         {ProfessionalTemp?.map((items, index) => (
           <>
-            <Box className="cursor" key={index}>
+            <Box className="cursor" key={index} mx={[0, 0, 0, 0, 6]}>
               <Link onClick={() => {
                 router.push(items?.href)
               }} style={{ cursor: "default" }}
@@ -53,51 +54,49 @@ function Templates() {
                   textDecoration: 'none'
                 }}
               >
-                <div className="templatecontainer">
-                  <div class="templatecontainer">
-                    <Image
-                      className="templateimage"
-                      src={items?.src}
-                      alt={items?.alt}
-                      height={items?.height}
-                      width={items?.width}
-                    />
-                    <div className="templatemiddle">
-                      <div className="templatetext">Create my Resume</div>
-                    </div>
-                    <Box
-                      display={'flex'}
-                      margin={'20px 0px 0px 0px'}
-                      width={'87%'}
-                    >
-                      <Text
-                        fontSize={'1.3rem'}
-                        fontStyle={'normal'}
-                        fontFamily={`poppins-bold`}
-                        margin={'20px 0px'}
-                        color={'#313B47'}
-                      >
-                        {items?.category?.categoryName}
-                      </Text>
-                      <Text
-                        fontSize={'0.92rem'}
-                        fontStyle={'normal'}
-                        fontFamily={`poppins`}
-                        margin={'22px 0px 20px 20px'}
-                        color={'#757575'}
-                      >
-                        {items?.category?.categoryID}
-                        {"/"}
-                        {items?.category?.totalCategory}
-                      </Text>
-                    </Box>
+                <div class="templatecontainer">
+                  <Image
+                    className="templateimage"
+                    src={items?.src}
+                    alt={items?.alt}
+                    height={items?.height}
+                    width={items?.width}
+                  />
+                  <div className="templatemiddle">
+                    <div className="templatetext">Create my Resume</div>
                   </div>
                 </div>
+                <Box
+                  display={'flex'}
+                  margin={'20px 0px 30px 0px'}
+                  alignItems={'center'}
+                >
+                  <Text
+                    fontSize={'1.3rem'}
+                    fontStyle={'normal'}
+                    fontFamily={`poppins-bold`}
+                    margin={'10px 0px 0px 0px'}
+                    color={'#313B47'}
+                  >
+                    {items?.category?.categoryName}
+                  </Text>
+                  <Text
+                    fontSize={'0.92rem'}
+                    fontStyle={'normal'}
+                    fontFamily={`poppins`}
+                    margin={'10px 0px 0px 20px'}
+                    color={'#757575'}
+                  >
+                    {items?.category?.categoryID}
+                    {"/"}
+                    {items?.category?.totalCategory}
+                  </Text>
+                </Box>
               </Link>
             </Box>
           </>
         ))}
-      </SimpleGrid>
+      </Container>
 
       <Box
         display={'flex'}
@@ -115,15 +114,16 @@ function Templates() {
           Modern
         </Text>
       </Box>
-      <SimpleGrid
-        columns={{ base: 2, sm: 3, md: 4 }}
-        spacing={10}
+      <Container
+        maxWidth={'100%'}
+        display={'flex'}
+        flexWrap={'wrap'}
+        justifyContent={['center', 'center', 'space-evenly', 'space-evenly']}
         py={4}
-        px={10}
       >
         {Modern?.map((items, index) => (
           <>
-            <Box className="cursor" key={index}>
+            <Box className="cursor" key={index} mx={[0, 0, 0, 0, 6]}>
               <Link onClick={() => {
                 isUserLoggedIn ?
                   router.push(items?.href)
@@ -148,14 +148,14 @@ function Templates() {
                 </div>
                 <Box
                   display={'flex'}
-                  margin={'20px 0px 0px 0px'}
-                  width={'87%'}
+                  margin={'20px 0px 30px 0px'}
+                  alignItems={'center'}
                 >
                   <Text
                     fontSize={'1.3rem'}
                     fontStyle={'normal'}
                     fontFamily={`poppins-bold`}
-                    margin={'20px 0px'}
+                    margin={'10px 0px 0px 0px'}
                     color={'#313B47'}
                   >
                     {items?.category?.categoryName}
@@ -164,7 +164,7 @@ function Templates() {
                     fontSize={'0.92rem'}
                     fontStyle={'normal'}
                     fontFamily={`poppins`}
-                    margin={'22px 0px 20px 20px'}
+                    margin={'10px 0px 0px 20px'}
                     color={'#757575'}
                   >
                     {items?.category?.categoryID}
@@ -176,7 +176,7 @@ function Templates() {
             </Box>
           </>
         ))}
-      </SimpleGrid>
+      </Container>
     </Box>
   );
 }
