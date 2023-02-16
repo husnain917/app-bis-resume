f998627a69a8fc3771dff4a5a4a09aa1b403b77c
Ahsan Ali <ahsanbutt515@icloud.com>
2023-02-16 12:11:16 +0500

    UpdateCode

--- src/templates/AhsanTemplate1.js
+++ src/templates/AhsanTemplate1.js
@@ -84,7 +84,7 @@ const AhsanTemplate1 = () => {
                 FNameStyle={Classes.Name}
                 SNameStyle={Classes.Name}
                 minWidth={"180px"}
-                maxWidth={"300px"}
+                maxWidth={"380px"}
                 maxChr={38}
                 fontColor={color ? color : ""}
               />
